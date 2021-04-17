cd277a1d35db4a07f037c68e0575c4631e8d74cc
Ronald Rodrigues Farias <ronald-farias@outlook.com>
2021-04-16 21:25:09 -0300

    feat: add delete and cmd parsers interface

--- src/todosh_notion/todonotion.py
+++ src/todosh_notion/todonotion.py
@@ -1,25 +1,49 @@
 import argparse
 
 from notion.client import NotionClient
-
-from todosh_notion.settings import TASK_LIST_URL, TOKEN_V2
+from settings import TASK_LIST_URL, TOKEN_V2
 
 client = NotionClient(token_v2=TOKEN_V2)
 page = client.get_collection_view(TASK_LIST_URL)
 
 
-def add(text):
+def add(task: str) -> None:
     row = page.collection.add_row()
-    row.title = text
+    row.title = task
     row.status = "To Do"
 
 
-def done(text):
-    for row in page.collection.get_rows(search=text):
+def done(task: str) -> None:
+    for row in page.collection.get_rows(search=task):
         row.status = "Done"
 
 
+def delete(task: str) -> None:
+    for row in page.collection.get_rows(search=task):
+        row.remove()
+
+
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Manage cards into Notion task list")
-    parser.add_argument("add", help="Add a task in To Do")
-    parser.add_argument("done", help="Mark the task as Done")
+    parser = argparse.ArgumentParser(description="Manage cards in Notion task list")
+    subparsers = parser.add_subparsers(title="actions")
+
+    parser_add = subparsers.add_parser(
+        "add", add_help=False, help="Add a task in To Do"
+    )
+    parser_add.add_argument("task")
+    parser_add.set_defaults(func=delete)
+
+    parser_done = subparsers.add_parser(
+        "done", add_help=False, help="Mark the task as Done"
+    )
+    parser_done.add_argument("task")
+    parser_done.set_defaults(func=delete)
+
+    parser_delete = subparsers.add_parser(
+        "delete", add_help=False, help="Delete the task"
+    )
+    parser_delete.add_argument("task")
+    parser_delete.set_defaults(func=delete)
+
+    argx = parser.parse_args()
+    argx.func(argx.task)
