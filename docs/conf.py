2e25ea19c01fbbe91b6edbfbed11631c942cf7ae
Ronald Rodrigues Farias <ronald-farias@outlook.com>
2021-04-19 11:05:10 -0300

    feat: rtd theme for docs

--- docs/conf.py
+++ docs/conf.py
@@ -11,6 +11,7 @@ import os
 import sys
 import inspect
 import shutil
+import sphinx_rtd_theme
 
 # -- Path setup --------------------------------------------------------------
 
@@ -78,6 +79,7 @@ extensions = [
     "sphinx.ext.ifconfig",
     "sphinx.ext.mathjax",
     "sphinx.ext.napoleon",
+    "sphinx_rtd_theme",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -168,7 +170,7 @@ pygments_style = "sphinx"
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = "alabaster"
+html_theme = "sphinx_rtd_theme"
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
